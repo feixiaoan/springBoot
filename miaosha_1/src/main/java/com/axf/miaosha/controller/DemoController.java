1de55dfada2942fafcf7f823f88139a7903c836c
feixiaoan <43228179+feixiaoan@users.noreply.github.com>
2019-04-11 15:03:04 +0800

    springBoot1
    springboot测试

---
+++ miaosha_1/src/main/java/com/axf/miaosha/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.axf.miaosha.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class DemoController {
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+	}
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(DemoController.class,args);
+	}
+}
